b4bdc1b55e656bbb703ecf5c46e7472be548d067
Marius Mandal <mariusmandal@gmail.com>
2018-03-15 10:04:23 +0100

    Oppdatert oppsummering

--- controller/oppsummering.controller.php
+++ controller/oppsummering.controller.php
@@ -1,274 +1,119 @@
 <?php
+
+$summering = [];
+foreach( UKMVideresending::getTil() as $monstring ) {
 	
-$monstring_fra = UKMVideresending::getFra();
-$season = $monstring_fra->getSesong();
+	/**
+	 * SETT OPP MØNSTRINGS-OBJEKTET
+	 * Lag et array for summering
+	 * Finn ut hvilke kommuner denne mønstringen har
+	**/
 
-// TOTAL
-$kvote_param[]	= (object) array(	'id' 		=> 'total_personer',
-									'verdi'		=>	(int) get_site_option('UKMFvideresending_kvote_deltakere_'.$season),
-									'tittel'	=> 'Totalt antall deltakere',
-									'enhet'		=>	'person',
-									'flertall'	=> 'er'
-								);
+	// INITIER SUM-OBJEKT FOR MØNSTRINGEN
+	$sum_monstring = 
+		[
+			'personer' => [],
+			'scene' => [
+				'innslag' 	=> 0,
+				'personer'	=> 0,
+				'titler'	=> 0,
+				'varighet'	=> 0,
+			],
+			'total' => [
+				'innslag' 	=> 0,
+				'personer'	=> 0,
+				'titler'	=> 0,
+				'varighet'	=> 0,
+				'unike'		=> 0,
+			],
+		];
 
-if(get_option('site_type' == 'fylke')) {						
-	$kvote_param[]	= (object) array(	'id' 		=> 'total_ledere',
-										'verdi'		=>	(int) get_site_option('UKMFvideresending_kvote_ledere_'.$season),
-										'tittel'	=> 'Antall ledere',
-										'enhet'		=>	'person',
-										'flertall'	=> 'er'
-									);
-						
-	$kvote_param[]	= (object) array(	'id' 		=> 'total_deltakere_per_leder',
-										'verdi'		=>	10,
-										'tittel'	=> 'Antall deltakere per leder',
-										'enhet'		=>	'deltaker',
-										'flertall'	=> 'e'
-									);
-}
-// SCENE
-$kvote_param[]	= (object) array(	'id' 		=> 'scene_antall',
-									'verdi'		=>	5,
-									'tittel'	=> 'Antall sceneinnslag',
-									'enhet'		=>	'innslag',
-									'flertall'	=> ''
-								);
-						
-$kvote_param[]	= (object) array(	'id' 		=> 'scene_varighet',
-									'verdi'		=>	25,
-									'tittel'	=> 'Varighet sceneinnslag',
-									'enhet'		=>	'minutt',
-									'flertall'	=> 'er'
-								);
-
-// FILM
-$kvote_param[]	= (object) array(	'id' 		=> 'film_antall',
-									'verdi'		=>	1,
-									'tittel'	=> 'Antall filmer',
-									'enhet'		=>	'film',
-									'flertall'	=> 'er',
-									'kommentar' => '2 filmer hvis total varighet er under 6 minutter'
-								);
-
-$kvote_param[]	= (object) array(	'id' 		=> 'film_varighet',
-									'verdi'		=>	5,
-									'tittel'	=> 'Varighet filmer',
-									'enhet'		=>	'minutt',
-									'flertall'	=>  'er',
-									'kommentar' => '2 filmer gir utvidet kvote: 6 minutter'
-								);
-
-$kvote_param[]	= (object) array(	'id' 		=> 'film_personer',
-									'verdi'		=>	2,
-									'tittel'	=> 'Antall filmskapere',
-									'enhet'		=>	'person',
-									'flertall'	=> 'er',
-									'kommentar' => '2 filmer gir utvidet kvote: 4 personer'
-								);
-
-// KUNST
-$kvote_param[]	= (object) array(	'id' 		=> 'kunst_antall',
-									'verdi'		=>	4,
-									'tittel'	=> 'Antall kunstverk',
-									'enhet'		=>	'verk',
-									'flertall'	=> ''
-								);
-
-$kvote_param[]	= (object) array(	'id' 		=> 'kunst_personer',
-									'verdi'		=>	8,
-									'tittel'	=> 'Antall kunstnere',
-									'enhet'		=>	'person',
-									'flertall'	=> 'er'
-								);
-
-// TITTELLØSE
-$kvote_param[]	= (object) array(	'id' 		=> 'nettredaksjon_antall',
-									'verdi'		=>	'nomineres',
-									'tittel'	=> 'Antall nettredaksjon',
-									'enhet'		=>	'person',
-									'flertall'	=> 'er'
-								);
-
-$kvote_param[]	= (object) array(	'id' 		=> 'arrangor_antall',
-									'verdi'		=>	'nomineres',
-									'tittel'	=> 'Antall arrangører',
-									'enhet'		=>	'person',
-									'flertall'	=> 'er'
-								);
-
-$kvote_param[]	= (object) array(	'id' 		=> 'konferansier_antall',
-									'verdi'		=>	'nomineres',
-									'tittel'	=> 'Antall konferansierer',
-									'enhet'		=>	'person',
-									'flertall'	=> 'er'
-								);
-
-
-// SET DEFAULT STATES
-foreach( $kvote_param as $param ) {
-	foreach( UKMVideresending::getTil() as $monstring_til ) {
-		$geo_key = 
-			$monstring_til->getType() == 'fylke' ? 
-				$monstring_til->getFylke()->getId() :
-				0
-			;
-		$videresendte[ $geo_key ][ $param->id ] = 0;
+	// HVILKE KOMMUNER SKAL INKLUDERES I DENNE OVERSIKTEN
+	if( UKMVideresending::getFra()->getType() == 'kommune' ) {
+		$kommuner = [];
+		foreach(  UKMVideresending::getFra()->getKommuner() as $kommune ) {
+			$kommuner[] = $kommune->getId();
+		}
 	}
-	$TWIG['kvoter'][ $param->id ] = $param;
-}
-
-// HVILKE KOMMUNER SKAL INKLUDERES I DENNE OVERSIKTEN
-$fra_monstring = UKMVideresending::getFra();
-if( $fra_monstring->getType() == 'kommune' ) {
-	$kommuner = [];
-	foreach( $fra_monstring->getKommuner() as $kommune ) {
-		$kommuner[] = $kommune->getId();
-	}
-}
-
-
-// LOOP ALLE MØNSTRINGER VI KAN VIDERESENDE TIL
-$monstringer = UKMVideresending::getTil();
-foreach( $monstringer as $monstring ) {
-	// LOOP ALLE INNSLAG I MØNSTRINGEN
+	
+	/**
+	 * LOOP ALLE INNSLAG I MOTTAKER-MØNSTRINGEN
+	 * Tell kun de som er fra kommunene i denne mønstringen (array $kommuner)
+	**/
 	foreach( $monstring->getInnslag()->getAll() as $innslag ) {
-		
 		// KUN VIS INNSLAGENE SOM KOMMER FRA DENNE MØNSTRINGEN
-		if( $fra_monstring->getType() == 'kommune' && !in_array( $innslag->getKommune()->getId(), $kommuner ) ) {
+		if( UKMVideresending::getFra()->getType() == 'kommune' && !in_array( $innslag->getKommune()->getId(), $kommuner ) ) {
 			continue;
 		}
 		
-		if( $monstring->getType() == 'land' ) {
-			if( $monstring_fra->getType() == 'fylke' && $innslag->getFylke()->getId() != $monstring_fra->getFylke()->getId() ) {
-				continue;
-			}
+		echo $innslag->getNavn() .' '. $innslag->getId() .'<br />';
+		
+		// SET SORT-KEY FOR DETTE INNSLAGET (ALTSÅ HVOR LAGRES SUMMEN?)
+		$sort_key = $innslag->getType()->getKey() == 'scene' ? 'annet' : $innslag->getType()->getKey();
+		
+		// INITIER DATA-ARRAY
+		if( !isset( $sum_monstring[ $sort_key ] ) ) {
+			$sum_monstring[ $sort_key ] = [
+				'innslag'	=> 0,
+				'personer'	=> 0,
+				'titler'	=> 0,
+				'varighet'	=> 0,
+			];
 		}
 		
-		// SETT GEO-NØKKEL FOR INFORMASJON OM DENNE MØNSTRINGEN
-		$geo_id = $innslag->getFylke()->getId();
-
-		// Scene-innslag
-		if( $innslag->getType()->getId() == 1 ) {
-			$videresendte[ $geo_id ]['scene_antall']++;
-			$videresendte[ $geo_id ]['scene_varighet'] += $innslag->getVarighet()->getSekunder();
-			
-			foreach( $innslag->getPersoner()->getAll() as $person ) {
-				$unike_personer[ $geo_id ]['scene'][ $person->getId() ] = $person;
-			}
+		// INNSLAG
+		$sum_monstring[ $sort_key ]['innslag'] += 1;
+		
+		// PERSONER
+		foreach( $innslag->getPersoner()->getAllVideresendt( $monstring ) as $person ) {
+			$sum_monstring[ $sort_key ]['personer'] ++;
+			$sum_monstring['personer'][] = $person->getId();
 		}
 		
-		// Film-innslag
-		elseif( $innslag->getType()->getKey() == 'video' ) {
-			$videresendte[ $geo_id ]['film_antall'] += $innslag->getTitler()->getAntall();
-			$videresendte[ $geo_id ]['film_varighet'] += $innslag->getVarighet()->getSekunder();
+		// VARIGHET
+		if( $innslag->getType()->harTitler() ) {
+			$sum_monstring[ $sort_key ]['titler'] 	+= $innslag->getTitler()->getAntall();
+			$sum_monstring[ $sort_key ]['varighet'] 	+= $innslag->getVarighet()->getSekunder();
+		}
+	}
 	
-			// 2 filmer gir totalt 6 min varighet og 4 pers
-			if( $videresendte[ $geo_id ]['film_antall'] > 1 ) {
-				$TWIG['kvoter']['film_varighet']->verdi = 6;
-				$TWIG['kvoter']['film_personer']->verdi = 4;
-			}
-			
-			foreach( $innslag->getPersoner()->getAll() as $person ) {
-				$unike_personer[ $geo_id ]['film'][ $person->getId() ] = $person;
-			}
-		}
-		
-		// KUNST
-		elseif( $innslag->getType()->getKey() == 'utstilling' ) {
-			$videresendte[ $geo_id ]['kunst_antall'] += $innslag->getTitler()->getAntall();
-	
-			foreach( $innslag->getPersoner()->getAll() as $person ) {
-				$unike_personer[ $geo_id ]['kunst'][ $person->getId() ] = $person;
-			}
-		}
-		
-		// KONFERANSIER
-		elseif( $innslag->getType()->getKey() == 'konferansier' ) {
-			$videresendte[ $geo_id ]['konferansier_antall']++;
-			
-			$person = $innslag->getPersoner()->getSingle();
-			$unike_personer[ $geo_id ]['tittellose'][ $person->getId() ] = $person;
-		}
-		
-		// MEDIA
-		elseif( $innslag->getType()->getKey() == 'nettredaksjon' ) {
-			$videresendte[ $geo_id ]['nettredaksjon_antall']++;
-	
-			$person = $innslag->getPersoner()->getSingle();
-			$unike_personer[ $geo_id ]['tittellose'][ $person->getId() ] = $person;
-		}
-		
-		// ARRANGØR
-		elseif( $innslag->getType()->getKey() == 'arrangor' ) {
-			$videresendte[ $geo_id ]['arrangor_antall']++;
-	
-			$person = $innslag->getPersoner()->getSingle();
-			$unike_personer[ $geo_id ]['tittellose'][ $person->getId() ] = $person;
-		}
-		
-		// MATKULTUR
-		elseif( $innslag->getType()->getKey() == 'matkultur' ) {
-			$videresendte[ $geo_id ]['matkultur']++;
-			
-			foreach( $innslag->getPersoner()->getAll() as $person ) {
-				$unike_personer[ $geo_id ]['matkultur'][ $person->getId() ] = $person;
+	/**
+	 * SUMMER OPP KATEGORIENE OG TOTALEN
+	**/
+	// SUMMER ALLE SCENE-KATEGORIER TIL EN SCENE-VARIABEL
+	foreach( innslag_typer::getAllScene() as $scene_kategori ) {
+		$sort_key = $scene_kategori->getKey() == 'scene' ? 'annet' : $scene_kategori->getKey();
+		if( is_array( $sum_monstring[ $sort_key ] ) ) {
+			foreach( $sum_monstring[ $sort_key ] as $key => $val ) {
+				$sum_monstring['scene'][ $key ] += $val;
 			}
 		}
 	}
 	
-	$geo_id_monstring = 
-		$monstring->getType() == 'fylke' ? 
-			$monstring->getFylke()->getId() :
-			0
-		;
-	// OPPSUMMERING	
-	$videresendte[ $geo_id_monstring ]['kunst_personer'] 	= sizeof( $unike_personer[ $geo_id_monstring ]['kunst'] );
-	$videresendte[ $geo_id_monstring ]['film_personer'] 	= sizeof( $unike_personer[ $geo_id_monstring ]['film'] );
-	$videresendte[ $geo_id_monstring ]['scene_personer'] 	= sizeof( $unike_personer[ $geo_id_monstring ]['scene'] );
+	// SUMMER ALT TIL EN TOTAL, BRUK DISSE KATEGORIENE
+	$summeres = [
+		'scene',
+		'utstilling',
+		'video',
+		'arrangor',
+		'konferansier',
+		'nettredaksjon'
+	];
 	
-	$videresendte[ $geo_id_monstring ]['total_personer'] 	= 
-		sizeof($unike_personer[ $geo_id_monstring ]['tittellose']) + 
-		sizeof( $unike_personer[ $geo_id_monstring ]['kunst'] ) + 
-		sizeof( $unike_personer[ $geo_id_monstring ]['film'] ) + 
-		sizeof( $unike_personer[ $geo_id_monstring ]['scene'] ) + 
-		sizeof( $unike_personer[ $geo_id_monstring ]['matkultur'] );
-	
-	$videresendte[ $geo_id_monstring ]['scene_tid'] 		= $videresendte[ $geo_id_monstring ]['scene_varighet'];
-	$videresendte[ $geo_id_monstring ]['film_tid'] 			= $videresendte[ $geo_id_monstring ]['film_varighet'];
-	
-}
-
-UKMVideresending::addViewData('til', UKMVideresending::getTil() );
-UKMVideresending::addViewData('kvoter', $TWIG['kvoter']);
-UKMVideresending::addViewData('info1', get_site_option('UKMFvideresending_info1_'.$season));
-UKMVideresending::addViewData('videresendt', $videresendte);
-
-
-if( $monstring->getType() == 'land' ) {
-	// LEDERE
-	$ledere = new SQL("SELECT COUNT(`l_id`) AS `num_ledere`
-						FROM `smartukm_videresending_ledere_ny`
-						WHERE `pl_id_from` = '#pl_from'
-						AND `pl_id_to` = '#pl_to'
-						AND `season` = '#season'
-						ORDER BY `l_navn` ASC",
-					array(	'pl_from' 	=> $monstring_fra->getId(),
-							'pl_to' 	=> UKMVideresending::getTil()[0]->getId(),
-							'season' 	=> UKMVideresending::getTil()[0]->getSesong(),
-						)
-					);
-	$videresendte['total_ledere'] = $ledere->run('field','num_ledere');
-	if( $videresendte['total_personer'] > 0 && $videresendte['total_ledere'] > 0) {
-		$videresendte['total_deltakere_per_leder'] = ceil( $videresendte['total_personer'] / $videresendte['total_ledere'] );
-	} else { 
-		$videresendte['total_deltakere_per_leder'] = $videresendte['total_personer'];
+	foreach( $summeres as $kategori ) {
+		if( isset( $sum_monstring[ $kategori ] ) ) {
+			foreach( $sum_monstring[ $kategori ] as $key => $val ) {
+				$sum_monstring['total'][ $key ] += $val;
+			}
+		}
 	}
-/* TODO:	
-	update_infoskjema_field( 
-		$monstring_fra->getId(), 
-		UKMVideresending::getTil()[0]->getId(), 
-		'systemet_overnatting_spektrumdeltakere', 
-		$videresendte['total_personer']
-	);
-*/
+	
+	// HVIS PERSONER ER VIDERESENDT, KALKULER ANTALL UNIKE
+	if( is_array( $sum_monstring['personer'] ) ) {
+		$sum_monstring['total']['unike'] = sizeof( array_unique( $sum_monstring['personer'] ) );
+	}
+	
+	$summering[ $monstring->getId() ] = $sum_monstring;
 }
+
+UKMVideresending::addViewData('summering', $summering);
