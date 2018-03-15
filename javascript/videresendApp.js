99b9abae353bd1a4527b0523b31c4677f8a871fa
Marius Mandal <mariusmandal@gmail.com>
2018-03-15 11:58:27 +0100

    ie?

--- javascript/videresendApp.js
+++ javascript/videresendApp.js
@@ -70,7 +70,7 @@ var UKMVideresending = function( $ ) {
 					$(this).attr('data-innslag'),
 					$(this).attr('data-id')
 				);
-				if( item.erVideresendt() ) {
+				if( item != false && item.erVideresendt() ) {
 					item.setVideresendt();
 				}
 			});
