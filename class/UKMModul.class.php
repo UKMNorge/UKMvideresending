9bb89a527d54ea67dd901cdddcc621bc6f2c73d2
Marius Mandal <mariusmandal@gmail.com>
2018-05-29 11:29:13 +0200

    legg til setAction og if( !class_exists )

--- class/UKMModul.class.php
+++ class/UKMModul.class.php
@@ -1,76 +1,83 @@
 <?php
-class UKMmodul {
-	public static $view_data = null;
-	public static $ajax_response = null;
-	public static $action = null;
-	
-	public static function init( $pl_id ) {
-		if( isset( $_GET['action'] ) ) {
-			self::$action = $_GET['action'];
-		}
-	}
-
-	/**
-	 * Hent hvilken viewAction som er aktive
-	 *
-	 * @return string
-	**/
-	public static function getAction() {
-		return self::$action;
-	}
+if( !class_exists('UKMmodul') ) {
+	class UKMmodul {
+		public static $view_data = null;
+		public static $ajax_response = null;
+		public static $action = null;
 		
-	/**
-	 * Hent alle view-data
-	 *
-	 * @return array
-	**/
-	public static function getViewData() {
-		return self::$view_data;
-	}
-
-	/**
-	 * Legg til viewdata
-	 * 
-	 * Tar i mot array med flere keys (ett parameter)
-	 * eller key, value (to parameter)
-	 *
-	 * @param [string|array] key eller [key => val]
-	 * @param [null|array] data hvis oppgitt key som string
-	 * @return void
-	**/
-	public static function addViewData( $key_or_array, $data=null ) {
-		if( is_array( $key_or_array ) ) {
-			self::$view_data = array_merge( self::$view_data, $key_or_array );
-		} else {
-			self::$view_data[ $key_or_array ] = $data;
+		public static function init( $pl_id=null ) {
+			self::$view_data = [];
+			if( isset( $_GET['action'] ) ) {
+				self::setAction( $_GET['action'] );
+			}
 		}
-	}
 	
-	/**
-	 * Hent alle ajax response-data
-	 *
-	 * @return array
-	**/
-	public static function getResponseData() {
-		return self::$ajax_response;
-	}
-
-	/**
-	 * Legg til ajax respons-data
-	 * 
-	 * Tar i mot array med flere keys (ett parameter)
-	 * eller key, value (to parameter)
-	 *
-	 * @param [string|array] key eller [key => val]
-	 * @param [null|array] data hvis oppgitt key som string
-	 * @return void
-	**/
-	public static function addResponseData( $key_or_array, $data=null ) {
-		if( is_array( $key_or_array ) ) {
-			self::$ajax_response = array_merge( self::$ajax_response, $key_or_array );
-		} else {
-			self::$ajax_response[ $key_or_array ] = $data;
+		/**
+		 * Hent hvilken viewAction som er aktive
+		 *
+		 * @return string
+		**/
+		public static function getAction() {
+			return self::$action;
 		}
+		
+		public static function setAction( $action ) {
+			self::$action = $action;
+		}
+			
+		/**
+		 * Hent alle view-data
+		 *
+		 * @return array
+		**/
+		public static function getViewData() {
+			return self::$view_data;
+		}
+	
+		/**
+		 * Legg til viewdata
+		 * 
+		 * Tar i mot array med flere keys (ett parameter)
+		 * eller key, value (to parameter)
+		 *
+		 * @param [string|array] key eller [key => val]
+		 * @param [null|array] data hvis oppgitt key som string
+		 * @return void
+		**/
+		public static function addViewData( $key_or_array, $data=null ) {
+			if( is_array( $key_or_array ) ) {
+				self::$view_data = array_merge( self::$view_data, $key_or_array );
+			} else {
+				self::$view_data[ $key_or_array ] = $data;
+			}
+		}
+		
+		/**
+		 * Hent alle ajax response-data
+		 *
+		 * @return array
+		**/
+		public static function getResponseData() {
+			return self::$ajax_response;
+		}
+	
+		/**
+		 * Legg til ajax respons-data
+		 * 
+		 * Tar i mot array med flere keys (ett parameter)
+		 * eller key, value (to parameter)
+		 *
+		 * @param [string|array] key eller [key => val]
+		 * @param [null|array] data hvis oppgitt key som string
+		 * @return void
+		**/
+		public static function addResponseData( $key_or_array, $data=null ) {
+			if( is_array( $key_or_array ) ) {
+				self::$ajax_response = array_merge( self::$ajax_response, $key_or_array );
+			} else {
+				self::$ajax_response[ $key_or_array ] = $data;
+			}
+		}
+	
 	}
-
 }
