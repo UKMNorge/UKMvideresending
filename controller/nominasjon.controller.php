2f983f8e986b08f7d5a6bb8aab2f820d1524c1a4
Marius Mandal <mariusmandal@gmail.com>
2020-03-07 18:45:49 +0100

    Fungerende nominasjon

--- controller/nominasjon.controller.php
+++ controller/nominasjon.controller.php
@@ -1,26 +1,9 @@
 <?php
 
+use UKMNorge\Innslag\Typer\Typer;
+
 if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
     UKMVideresending::require('save/nominasjon.save.php');
 }
 
-UKMVideresending::addViewData(
-	'nominasjonstyper',
-	[
-		'arrangor' => 'Arrangører',
-		'konferansier' => 'Konferansierer',
-		'nettredaksjon' => 'UKM Media',
-	]
-);
-
 $fra = UKMVideresending::getFra();
-$alle_innslag = [];
-foreach( $fra->getInnslag()->getAll() as $innslag ) {
-	// Vis kun arrangører, media og konferansierer i listen
-	if( !in_array( $innslag->getType()->getId(), [4,5,8] ) ) {
-		continue;
-	}
-    $alle_innslag[ $innslag->getType()->getKey() ][] = $innslag;
-}
-
-UKMVideresending::addViewData('innslag', $alle_innslag);
