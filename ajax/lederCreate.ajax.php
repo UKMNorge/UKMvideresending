148b3bd6b67a13e984f534f71d5718c7cecc324f
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2021-03-18 13:31:36 +0100

    Konverterer date til Javascript og itererer i Twig. Fix av table head
    Close #32

--- ajax/lederCreate.ajax.php
+++ ajax/lederCreate.ajax.php
@@ -10,12 +10,7 @@ $leder = new Leder( $fra->getId(), $til->getId() );
 $leder->setType('reise');
 $leder = Write::create($leder);
 
-$netter = [];
-foreach( $til->getArrangement()->getNetter() as $natt ) {
-    $netter[] = $natt->getTimestamp();
-}
-
 UKMVideresending::addResponseData('success', true);
-UKMVideresending::addResponseData('netter', $netter);
+UKMVideresending::addResponseData('netter', $til->getArrangement()->getNetter());
 UKMVideresending::addResponseData('overnattingssteder', UKMVideresending::getOvernattingssteder($til->getArrangement()));
 UKMVideresending::addResponseData('leder', $leder->getJsObject());
