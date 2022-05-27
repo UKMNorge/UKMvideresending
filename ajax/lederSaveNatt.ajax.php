059c9ff615239851797114c56e07e1d8bb87b116
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-05-27 18:15:36 +0200

    Hvis brukeren prøver å lagre natt utenfor hotell for sykerom
    Det tillates ikke å ha sykerom utenfor hotell for nå!

--- ajax/lederSaveNatt.ajax.php
+++ ajax/lederSaveNatt.ajax.php
@@ -5,8 +5,15 @@ use UKMNorge\Arrangement\Videresending\Ledere\Write;
 
 $leder = Leder::getById( intval( $_POST['leder'] ) );
 $natt = $leder->getNatt( $_POST['dato'] );
-$natt->setSted($_POST['sted'] );
 
-$res = Write::saveNatt($natt);
-
-UKMVideresending::addResponseData('success', !!$res );
+// Det tillates ikke at sykerom har et sted utenfor hotell
+if($leder->getType() == 'sykerom' && $_POST['sted'] != 'hotell') {
+    UKMVideresending::addResponseData('success', [] );
+}
+else {
+    $natt->setSted($_POST['sted'] );
+    
+    $res = Write::saveNatt($natt);
+    
+    UKMVideresending::addResponseData('success', !!$res );
+}
