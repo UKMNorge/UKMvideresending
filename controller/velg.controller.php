7448aac4304bb70a58d1df90eff8b94b8d1dbc45
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-02-27 20:37:18 +0100

    Sender UKMFestivalen
    Sender UKMFestivalen sender til velg.html.twig og videre til arrangementerTilgjengelig.html.twig

--- controller/velg.controller.php
+++ controller/velg.controller.php
@@ -1,3 +1,11 @@
 <?php
 
+use UKMNorge\Arrangement\UKMFestival;
+
+UKMVideresending::addViewData(
+    [
+        'UKMFestivalen' => UKMFestival::getCurrentUKMFestival(),
+    ]
+);
+
 UKMVideresending::require('controller/statistikk.controller.php');
