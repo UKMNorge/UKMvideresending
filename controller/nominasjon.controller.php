b83b01fb72653e61511210d84e23c950ee38eee8
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-03-23 11:58:16 +0100

    Bruk WP bruker på nominasjon
    Bruk WP bruker på nominasjon navn og telefon til voksen

--- controller/nominasjon.controller.php
+++ controller/nominasjon.controller.php
@@ -1,9 +1,13 @@
 <?php
 
 use UKMNorge\Innslag\Typer\Typer;
+use UKMNorge\Wordpress\User;
 
 if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
     UKMVideresending::require('save/nominasjon.save.php');
 }
+
+UKMVideresending::addViewData('wp_user_navn', wp_get_current_user()->display_name);
+UKMVideresending::addViewData('wp_user_phone', User::loadByEmail(wp_get_current_user()->user_email)->getPhone());
 
 $fra = UKMVideresending::getFra();
