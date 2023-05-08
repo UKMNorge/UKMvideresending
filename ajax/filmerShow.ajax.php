5ac7ffb9618876fae479a5087f6c96f4bea89308
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-05-08 17:00:44 +0200

    Hent filmer på innslag for arrangement på videresending

--- ajax/filmerShow.ajax.php
+++ ajax/filmerShow.ajax.php
@@ -4,10 +4,14 @@ $monstring = UKMVideresending::getFra();
 $innslag = $monstring->getInnslag()->get( $_POST['innslag'] );
 
 $filmer = [];
-foreach($innslag->getFilmer()->getAll() as $film) {
+foreach($innslag->getFilmer($monstring->getId())->getAll() as $film) {
     $f['embed_url'] = $film->getEmbedUrl();
     $filmer[] = $f;
 }
 
+// Antall filmer i innslag (kan være filmer fra andre arrangementer også)
+$antallFilmer = $innslag->getFilmer()->getAntall();
+
+UKMVideresending::addResponseData('antallFilmer', $antallFilmer);
 UKMVideresending::addResponseData('filmer', $filmer);
 UKMVideresending::addResponseData('success', true);
