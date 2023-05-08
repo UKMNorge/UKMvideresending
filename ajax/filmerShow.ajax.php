f69804b4109f762eabee78c08b08e824905560a1
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-05-08 13:21:01 +0200

    Sender filmer på riktig format på klient JS

--- ajax/filmerShow.ajax.php
+++ ajax/filmerShow.ajax.php
@@ -3,5 +3,11 @@
 $monstring = UKMVideresending::getFra();
 $innslag = $monstring->getInnslag()->get( $_POST['innslag'] );
 
-UKMVideresending::addResponseData('filmer', $innslag->getFilmer());
+$filmer = [];
+foreach($innslag->getFilmer()->getAll() as $film) {
+    $f['embed_url'] = $film->getEmbedUrl();
+    $filmer[] = $f;
+}
+
+UKMVideresending::addResponseData('filmer', $filmer);
 UKMVideresending::addResponseData('success', true);
