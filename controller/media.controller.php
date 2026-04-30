9f2aad1a67e91988c53507b7632ef1fd4e54e46e
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2026-04-30 15:40:59 +0200

    Media med nominasjoner

--- controller/media.controller.php
+++ controller/media.controller.php
@@ -1,4 +1,18 @@
 <?php
 
+use UKMNorge\Videresending\VideresendingNominasjon;
+use UKMNorge\Innslag\Innslag;
+
 $fra = UKMVideresending::getFra();
 $til = UKMVideresending::getValgtTil()->getArrangement();
+
+
+$nominerteInnslag = [];
+foreach (VideresendingNominasjon::getAlleTilArrangement($til->getId())->getAll() as $vNominasjon) {
+    $objNominasjon = $vNominasjon->getArrObj();
+
+    $innslag = Innslag::getById($objNominasjon['b_id']);
+    $nominerteInnslag[] = $innslag;
+}
+
+UKMVideresending::addViewData('nominerteInnslag', $nominerteInnslag);
